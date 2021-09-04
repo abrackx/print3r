1600aac304b6684407d29eb20679e3a95e4e78a9
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-09-04 17:30:27 -0400

    Added generate_verifier() and generate_challenge() functions as first steps of auth0 implementation.

--- server/src/auth/authentication.rs
+++ server/src/auth/authentication.rs
@@ -1,1 +1,15 @@
+use rand;
+use rand::Rng;
+use regex::Regex;
+use base64;
+use sha256;
 
+pub fn generate_verifier() -> String {
+    let random_bytes: [u8; 32] = rand::thread_rng().gen();
+    return base64::encode_config(random_bytes, base64::URL_SAFE_NO_PAD);
+}
+
+pub fn generate_challenge(verifier: String) -> String {
+    let hash = sha256::digest(verifier);
+    return base64::encode_config(hash, base64::URL_SAFE_NO_PAD);
+}
